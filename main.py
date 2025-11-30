2e2f90e74dca3a64bf3e7db108f2d0a9c3415ddb
NC4L4NGL4NG <calanglang.nea@obmontessori.edu.ph>
2025-12-01 04:40:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from pyscript import document
 
 def compute_gwa(e):
     try:
-        g1 = float(document.getElementById('Science').value)
-        g2 = float(document.getElementById('English').value)
-        g3 = float(document.getElementById('ICT').value)
-        g4 = float(document.getElementById('Math').value)
-        g5 = float(document.getElementById('Filipino').value)
+        g1 = float(document.getElementById('science').value)
+        g2 = float(document.getElementById('english').value)
+        g3 = float(document.getElementById('ict').value)
+        g4 = float(document.getElementById('math').value)
+        g5 = float(document.getElementById('filipino').value)
 
         gwa = (g1 + g2 + g3 + g4 + g5) / 5
-        display(f'Your GWA is {gwa:.2f}', target="output")
-    except Exception:
-        display(" Please enter valid numbers in all fields.", target="output")
 
+        result = document.getElementById("result")
+        result.textContent = f"Your GWA is: {gwa:.2f}"
 
-    output = document.getElementById("result")
-    output.textContent = f"Your GWA is: {gwa:.2f}"
+    except:
+        result = document.getElementById("result")
+        result.textContent = "Please enter valid numbers in all fields."
 
