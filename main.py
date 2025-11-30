bea369025b8b18fb94d16f1d7418b143c901c780
NC4L4NGL4NG <calanglang.nea@obmontessori.edu.ph>
2025-12-01 04:37:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
 from pyscript import document
 
-UNIT_SCIENCE = 5.0
-UNIT_ENGLISH = 3.0
-UNIT_ICT = 2.0
-UNIT_MATH = 5.0
-UNIT_FILIPINO = 3.0
+def compute_gwa(e):
+    try:
+        g1 = float(document.getElementById('Science').value)
+        g2 = float(document.getElementById('English').value)
+        g3 = float(document.getElementById('ICT').value)
+        g4 = float(document.getElementById('Math').value)
+        g5 = float(document.getElementById('Filipino').value)
 
-def compute_gwa():
-    grade_science = float(document.getElementById("science").value)
-    grade_english = float(document.getElementById("english").value)
-    grade_ict = float(document.getElementById("ict").value)
-    grade_math = float(document.getElementById("math").value)
-    grade_filipino = float(document.getElementById("filipino").value)
+        gwa = (g1 + g2 + g3 + g4 + g5) / 5
+        display(f'Your GWA is {gwa:.2f}', target="output")
+    except Exception:
+        display(" Please enter valid numbers in all fields.", target="output")
 
-    total_quality_points = (grade_science * UNIT_SCIENCE) + \
-                           (grade_english * UNIT_ENGLISH) + \
-                           (grade_ict * UNIT_ICT) + \
-                           (grade_math * UNIT_MATH) + \
-                           (grade_filipino * UNIT_FILIPINO)
-  
-    total_units = UNIT_SCIENCE + UNIT_ENGLISH + UNIT_ICT + UNIT_MATH + UNIT_FILIPINO
-    
-
-    gwa = total_quality_points / total_units
 
     output = document.getElementById("result")
     output.textContent = f"Your GWA is: {gwa:.2f}"
+
